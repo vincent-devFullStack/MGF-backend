97280290dcc8012f28d172737c495ba2aec6f9f0
Vincent S. <vincent.silvestri@hotmail.fr>
2025-03-10 11:23:53 +0100

    maj

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use("/", indexRouter);
 app.use("/eleve", eleveRouter);
